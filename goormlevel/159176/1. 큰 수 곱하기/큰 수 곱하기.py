dae47e8594c2f1b3afe9aac4e887f065eef7f5f9
thisischeese <80524511+thisischeese@users.noreply.github.com>
2024-10-05 21:43:06 +0900

    [난이도 2] Title: 큰 수 곱하기, Time: 0.02 ms, Memory: 10.06 MB -BaekjoonHub

---
+++ goormlevel/159176/1. 큰 수 곱하기/큰 수 곱하기.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# UTF-8 encoding when using korean
+import sys 
+
+N = list(map(int,sys.stdin.readline().split()))
+print(pow(N[0],len(N)))
+# 100,000,000,000,000,000,000 = 10^20
