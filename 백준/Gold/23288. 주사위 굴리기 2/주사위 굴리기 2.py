df1a767660431b7c5f7a55cac229fbd42e17bbf5
thisischeese <80524511+thisischeese@users.noreply.github.com>
2025-01-14 11:58:58 +0900

    Update 주사위 굴리기 2.py

--- 백준/Gold/23288. 주사위 굴리기 2/주사위 굴리기 2.py
+++ 백준/Gold/23288. 주사위 굴리기 2/주사위 굴리기 2.py
@@ -1,6 +1,7 @@
+import sys 
 from collections import deque
 
-def get_score(score, x, y):
+def update_score(score, x, y):
     visited = [[False] * m for _ in range(n)]
     queue = deque()
     queue.append((x, y))
@@ -24,49 +25,51 @@ def get_score(score, x, y):
     return score_sum
 
 
-def turn(direction, x, y):
+def turn_dice(dir, x, y):
     global dice
 
-    if direction == 0:
+    if dir == 0:
         dice = [dice[3], dice[1], dice[0], dice[5], dice[4], dice[2]]
 
-    elif direction == 1:
+    elif dir == 1:
         dice = [dice[1], dice[5], dice[2], dice[3], dice[0], dice[4]]
 
-    elif direction == 2:
+    elif dir == 2:
         dice = [dice[2], dice[1], dice[5], dice[0], dice[4], dice[3]]
 
     else:
         dice = [dice[4], dice[0], dice[2], dice[3], dice[5], dice[1]]
 
     if dice[-1] > data[x][y]:
-        direction = (direction + 1) % 4
+        dir = (dir + 1) % 4
     elif dice[-1] < data[x][y]:
-        direction = (direction - 1) % 4
+        dir = (dir - 1) % 4
 
-    return direction
+    return dir
 
-n, m, k = map(int, input().split())
-data = [list(map(int, input().split())) for _ in range(n)]
+n, m, k = map(int,sys.stdin.readline().split())
+data = []
+for _ in range(n):
+    data.append(list(map(int, sys.stdin.readline().split())))
 answer = 0
 
 dx = [0, 1, 0, -1]
 dy = [1, 0, -1, 0]
 
 dice = [1, 2, 3, 4, 5, 6]
-x, y, direction = 0, 0, 0
+x, y, dir = 0, 0, 0
 
 for _ in range(k):
-    nx = x + dx[direction]
-    ny = y + dy[direction]
+    nx = x + dx[dir]
+    ny = y + dy[dir]
 
     if 0 > nx or nx >= n or 0 > ny or ny >= m:
-        nx = x + dx[direction] * (-1)
-        ny = y + dy[direction] * (-1)
-        direction = (direction + 2) % 4
+        nx = x + dx[dir] * (-1)
+        ny = y + dy[dir] * (-1)
+        dir = (dir + 2) % 4
 
-    direction = turn(direction, nx, ny)
-    answer += get_score(data[nx][ny], nx, ny)
+    dir = turn_dice(dir, nx, ny)
+    answer += update_score(data[nx][ny], nx, ny)
 
     x, y = nx, ny
 
