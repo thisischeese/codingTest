bfd24282b8a1cc4c33711757e1cc79d528cf1acd
thisischeese <80524511+thisischeese@users.noreply.github.com>
2025-03-20 11:47:01 +0900

    [Silver II] Title: 유기농 배추, Time: 44 ms, Memory: 33992 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.py
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.py
@@ -1,46 +1,36 @@
 import sys
 sys.setrecursionlimit(10000)
 
-# 상, 하, 좌, 우 네 방향을 나타냄
-dx = [0, 0, -1, 1]
-dy = [-1, 1, 0, 0]
-
-# DFS로 인접한 배추들을 모두 탐색
-def dfs(x, y, farm, visited, M, N):
+def dfs(board, visited, x, y, M, N):
+    dx = [-1,1,0,0]
+    dy = [0,0,-1,1]
     visited[y][x] = True
     
     for i in range(4):
-        nx = x + dx[i]
-        ny = y + dy[i]
-        
-        # 농장 내에 있고, 배추가 있으며, 방문하지 않은 곳이면 DFS 재귀 호출
-        if 0 <= nx < M and 0 <= ny < N and farm[ny][nx] == 1 and not visited[ny][nx]:
-            dfs(nx, ny, farm, visited, M, N)
+        nx, ny = x + dx[i], y + dy[i]
+        if (0 <= nx < M and 0 <= ny < N and visited[ny][nx]==False and board[ny][nx] == 1):
+            dfs(board, visited, nx, ny, M, N)
 
-# 테스트 케이스 입력
-T = int(input())
+
+T = int(sys.stdin.readline())
+
 for _ in range(T):
-    # M: 가로, N: 세로, K: 배추 위치 개수
-    M, N, K = map(int, input().split())
-    
-    # 농장과 방문 여부를 저장할 리스트
-    farm = [[0] * M for _ in range(N)]
+    M, N, K = map(int, sys.stdin.readline().split())
+    board = [[0] * M for _ in range(N)]
     visited = [[False] * M for _ in range(N)]
     
-    # 배추 위치 입력
     for _ in range(K):
-        x, y = map(int, input().split())
-        farm[y][x] = 1  # 배추가 심어진 곳을 1로 표시
+        x, y = map(int, sys.stdin.readline().split())
+        board[y][x] = 1  
     
-    # 필요한 지렁이 수
-    worm_count = 0
+    answer = 0
     
-    # 농장을 순회하며 DFS 실행
-    for i in range(N):
-        for j in range(M):
-            if farm[i][j] == 1 and not visited[i][j]:
-                dfs(j, i, farm, visited, M, N)
-                worm_count += 1  # 새로운 컴포넌트가 발견될 때마다 지렁이 한 마리 추가
+    for row in range(N):
+        for col in range(M):
+            if (board[row][col] == 1 and visited[row][col] == False):
+                dfs(board, visited, col, row, M, N)
+                answer += 1
     
-    # 결과 출력
-    print(worm_count)
+    print(answer)
+
+    
