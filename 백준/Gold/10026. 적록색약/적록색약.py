7167bedd8c603f03432f0f5680c0fa5d56aff8a3
thisischeese <80524511+thisischeese@users.noreply.github.com>
2025-03-21 11:20:15 +0900

    Update 적록색약.py

--- 백준/Gold/10026. 적록색약/적록색약.py
+++ 백준/Gold/10026. 적록색약/적록색약.py
@@ -1,38 +1,40 @@
 import sys
-sys.setrecursionlimit(1000000)
+sys.setrecursionlimit(10000)
 
-N = int(sys.stdin.readline())
-grid = [list(sys.stdin.readline().rstrip()) for _ in range(N)]
-visited = [[False] * N for _ in range(N)]
+def dfs(x, y, color, grid, visited):
+    dx = [-1, 1, 0, 0]
+    dy = [0, 0, -1, 1]
+    
+    visited[x][y] = True
+    n = len(grid)
+    
+    for i in range(4):
+        nx, ny = x + dx[i], y + dy[i]
+        if (0 <= nx < n) and (0 <= ny < n) and not visited[nx][ny] and (grid[nx][ny] == color):
+            dfs(nx, ny, color, grid, visited)
 
-d = [(0,1), (0, -1), (1,0), (-1,0)]
-cnt, cnt2 = 0, 0
 
-def dfs(x, y):
-    visited[y][x] = True
-    color = grid[y][x]
-    for dx, dy in d:
-        X, Y = x + dx, y + dy
-        if (0 <= X < N) and (0 <= Y < N):
-            if visited[Y][X] == False and grid[Y][X] == color:
-                dfs(X, Y)
-            
-for y in range(N):
-    for x in range(N):
-        if visited[y][x] == False:
-            dfs(x,y)
-            cnt += 1
+N = int(sys.stdin.readline().strip())
+painting = [list(sys.stdin.readline().strip()) for _ in range(N)]
 
-for y in range(N):
-    for x in range(N):
-        if grid[y][x] == 'G':
-            grid[y][x] = 'R'
-visited = [[False] * N for _ in range(N)]
+visited_rgb = [[False] * N for _ in range(N)]
+visited_rb = [[False] * N for _ in range(N)]
 
-for y in range(N):
-    for x in range(N):
-        if visited[y][x] == False:
-            dfs(x,y)
-            cnt2 += 1
+# 적록색약 x
+cnt_rgb = 0
+for i in range(N):
+    for j in range(N):
+        if not visited_rgb[i][j]:
+            dfs(i, j, painting[i][j], painting, visited_rgb)
+            cnt_rgb += 1
 
-print(cnt, cnt2)
+# 적록색약 o 
+cnt_rb = 0
+painting_rb = [['R' if c == 'G' else c for c in row] for row in painting]
+for i in range(N):
+    for j in range(N):
+        if not visited_rb[i][j]:
+            dfs(i, j, painting_rb[i][j], painting_rb, visited_rb)
+            cnt_rb += 1
+
+print(cnt_rgb, cnt_rb)
