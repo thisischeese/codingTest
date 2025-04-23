4e0d904da749a4792a54f6a128bec4ed1c090d3a
thisischeese <80524511+thisischeese@users.noreply.github.com>
2025-04-23 12:31:09 +0900

    [Silver I] Title: 스티커, Time: 696 ms, Memory: 49684 KB -BaekjoonHub

--- 백준/Silver/9465. 스티커/스티커.py
+++ 백준/Silver/9465. 스티커/스티커.py
@@ -1,23 +1,15 @@
 import sys
-input = sys.stdin.readline
 
-T = int(input())
+T = int(sys.stdin.readline())
 for _ in range(T):
-    n = int(input())
-    sticker = [list(map(int, input().split())) for _ in range(2)]
+    n = int(sys.stdin.readline())
+    sticker = [list(map(int, sys.stdin.readline().split())) for _ in range(2)]
+    dp = [[0 for _ in range(n)] for _ in range(2)]
+    dp[0][0],dp[1][0] = sticker[0][0], sticker[1][0]
+    if n!=1:
+        dp[0][1],dp[1][1] = sticker[0][1]+sticker[1][0], sticker[1][1]+sticker[0][0]
+        for c in range(2,n):
+            dp[0][c] = max(dp[1][c-1],dp[1][c-2])+sticker[0][c]
+            dp[1][c] = max(dp[0][c-1],dp[0][c-2])+sticker[1][c]
     
-    if n == 1:
-        print(max(sticker[0][0], sticker[1][0]))
-        continue
-    
-    dp = [[0]*n for _ in range(2)]
-    dp[0][0] = sticker[0][0]
-    dp[1][0] = sticker[1][0]
-    dp[0][1] = dp[1][0] + sticker[0][1]
-    dp[1][1] = dp[0][0] + sticker[1][1]
-
-    for i in range(2, n):
-        dp[0][i] = max(dp[1][i-1], dp[1][i-2]) + sticker[0][i]
-        dp[1][i] = max(dp[0][i-1], dp[0][i-2]) + sticker[1][i]
-    
-    print(max(dp[0][n-1], dp[1][n-1]))
+    print(max(dp[0][-1],dp[1][-1]))
