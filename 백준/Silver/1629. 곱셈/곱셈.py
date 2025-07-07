9c75a212b99fdc222698b07ef1d078cccfacab7a
thisischeese <80524511+thisischeese@users.noreply.github.com>
2025-07-07 11:35:48 +0900

    [Silver I] Title: 곱셈, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1629. 곱셈/곱셈.py
+++ 백준/Silver/1629. 곱셈/곱셈.py
@@ -1,17 +1,4 @@
-import sys 
-
+import sys
 input = sys.stdin.readline
-
-A, B, C = map(int, input().split())
-
-def sol(A,B,C):
-    if B == 1:
-        return A%C
-    else:
-        temp = sol(A,B//2,C)
-        if B%2 == 0:
-            return temp*temp%C
-        else:
-            return temp*temp*A%C 
-            
-print(sol(A,B,C))
+a,b,c = map(int,input().split())
+print(pow(a,b,c))
