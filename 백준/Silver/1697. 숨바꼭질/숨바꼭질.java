0e45a7694d867a0acc2810b6cf266602705c4546
thisischeese <80524511+thisischeese@users.noreply.github.com>
2025-08-03 19:35:01 +0900

    [Silver I] Title: 숨바꼭질, Time: 232 ms, Memory: 109448 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
@@ -1,38 +1,39 @@
 import java.io.*; 
 import java.util.*; 
 
-public class Main {
+public class Main/*BJ_1697*/ {
 		
 	public static int bfs(int curr,int target) {
-		int answer = Integer.MAX_VALUE; 
 		boolean[] visited = new boolean[100001]; 
+		/*
+		// 불필요하게 2개의 큐를 사용할 필요가 없음 
 		Queue<Integer> queue = new LinkedList<>(); 
 		Queue<Integer> queue_time = new LinkedList<>(); 
-
-		queue.add(curr);
-		queue_time.add(0);
+		 */ 
+		// 하나의 큐에서 Pair로 위치와 시간을 같이 관리하기 
+		Queue<int[]> queue = new LinkedList<>(); 
+		queue.add(new int[] {curr,0}); 
 		
 		while(!queue.isEmpty()) {
-			int node = queue.poll();
-			int time = queue_time.poll(); 
-			visited[node]=true; 
+			int[] temp = queue.poll(); 
+			int node = temp[0]; 
+			int time = temp[1];
 			
 			if(node==target) {
-				answer = Math.min(answer, time);
+				return time;
 				}
-			
 			
 			int[] next = {node*2,node+1,node-1};
 			for(int n: next) {
-				if((0<=n & n<=100000) && !visited[n]) {
-					queue.add(n);
-					queue_time.add(time+1);
+				if(0<=n && n<=100000 && !visited[n]) {
+					visited[node]=true; 
+					queue.add(new int[] {n,time+1});
 				}
 			}
 			
 			
 		}
-		return answer;
+		return -1;
 	}
 	
 	public static void main(String[] args) throws Exception{
