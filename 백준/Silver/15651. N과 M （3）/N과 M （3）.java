f9efd3e201a1defe7246482cf97ae89f13684210
thisischeese <80524511+thisischeese@users.noreply.github.com>
2025-08-15 22:25:17 +0900

    [Silver III] Title: N과 M (3), Time: 364 ms, Memory: 112056 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.java
@@ -0,0 +1,37 @@
+import java.io.*; 
+import java.util.*; 
+
+public class Main {
+
+	public static void bt(int cnt,int N, int M, int[] temp, StringBuilder sb) {
+		if(cnt==M) {
+			for(int t : temp) {sb.append(t).append(" ");}
+			sb.append("\n");
+			return;
+				}
+		
+		for(int i=1;i<=N;i++) {
+			temp[cnt] = i; 
+			bt(cnt+1,N,M,temp,sb);
+		}
+	}
+	
+	public static void main(String[] args) throws Exception{
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder(); 
+		StringTokenizer st = new StringTokenizer(br.readLine()); 
+		
+		int N = Integer.parseInt(st.nextToken());		
+		int M = Integer.parseInt(st.nextToken());
+		//List<int[]> seq = new ArrayList<>();
+		int[] temp = new int[M]; 
+		//boolean[]v = new boolean[N+1]; v[0] = true; 
+		bt(0,N,M,temp, sb); 
+		
+		
+		br.close();
+		System.out.println(sb.toString());
+	}
+
+}
