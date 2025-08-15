cfd50cfba1c278c3f48713f57ba0d3592ecfe399
thisischeese <80524511+thisischeese@users.noreply.github.com>
2025-08-15 23:31:19 +0900

    [Silver III] Title: N과 M (5), Time: 216 ms, Memory: 31576 KB -BaekjoonHub

---
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.java
@@ -0,0 +1,49 @@
+import java.io.*; 
+import java.util.*; 
+
+public class Main {
+
+	public static void bt(int cnt,int idx,int N, int M, int[] temp,boolean[] v, int[] arr, StringBuilder sb) {
+		
+		if(cnt==M) {
+			for(int t : temp) {sb.append(t).append(" ");}
+			sb.append("\n");
+			return; 
+		}
+		
+		for(int i=0;i<N;i++) {
+			if(v[i]) {continue;}
+			temp[cnt] = arr[i]; 
+			v[i] = true; 
+			bt(cnt+1,i,N,M,temp,v,arr,sb);
+			v[i] = false; 
+			
+		}
+		
+	}
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder(); 
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken()); 
+		
+		int[] arr = new int[N]; 
+		int[] temp = new int[M];
+		boolean[] v = new boolean[N]; 
+		
+		st = new StringTokenizer(br.readLine());
+		for(int i=0;i<N;i++) {
+			arr[i] = Integer.parseInt(st.nextToken()); 
+		}
+
+		Arrays.sort(arr);
+		bt(0,0,N,M,temp,v,arr, sb);
+		
+		br.close();
+		System.out.println(sb.toString());
+	}
+
+}
