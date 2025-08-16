3394173ae734718df320d628de839798451a2c0c
thisischeese <80524511+thisischeese@users.noreply.github.com>
2025-08-16 21:52:47 +0900

    [Silver II] Title: N과 M (12), Time: 72 ms, Memory: 12076 KB -BaekjoonHub

---
+++ 백준/Silver/15666. N과 M （12）/N과 M （12）.java
@@ -0,0 +1,45 @@
+import java.io.*;
+import java.util.*; 
+
+public class Main {
+	
+	private static int N,M;
+	private static int[] temp,arr;
+	private static StringBuilder sb =  new StringBuilder();
+	
+	private static void bt(int cnt, int idx) {
+		if(cnt==M) {for(int t:temp) {sb.append(t).append(" ");}sb.append("\n"); return;}
+		int prev = -1;
+		
+		for(int i=idx;i<N;i++) {
+			if(i==idx) {prev = arr[i];}
+			else if(prev == arr[i]) {continue;}
+			
+			temp[cnt] = arr[i];
+			bt(cnt+1,i);
+			prev = arr[i];
+			
+		}
+	}
+	
+	public static void main(String[] args) throws Exception {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());	
+		arr= new int [N];
+		temp = new int[M];
+		
+		st = new StringTokenizer(br.readLine());
+		for(int i=0;i<N;i++) {arr[i] = Integer.parseInt(st.nextToken());}
+		Arrays.sort(arr);
+		bt(0,0);
+		
+		br.close();
+		System.out.println(sb.toString());
+		
+	}
+
+}
