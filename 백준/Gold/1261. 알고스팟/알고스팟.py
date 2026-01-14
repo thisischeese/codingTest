031638a2c53fd7447c0665cd57a4b697c4a5cc1d
thisischeese <iamclwm@gmail.com>
2026-01-14 13:59:36 +0900

    [Gold IV] Title: 알고스팟, Time: 68 ms, Memory: 34992 KB -BaekjoonHub

--- 백준/Gold/1261. 알고스팟/알고스팟.py
+++ 백준/Gold/1261. 알고스팟/알고스팟.py
@@ -1,5 +1,5 @@
 import sys 
-import heapq 
+from collections import deque 
 
 input = sys.stdin.readline 
 INF = 1e9 
@@ -7,32 +7,33 @@ INF = 1e9
 dr = [1,-1,0,0]
 dc = [0,0,1,-1]
 
+def BFS01(sr,sc,er,ec):
+    queue = deque() 
+    queue.append((sr,sc))
 
-def dijkstra(sr,sc,er,ec):
-
-    queue = [] 
-    heapq.heappush(queue,(int(graph[sr][sc]),sr,sc))
     distance[sr][sc] = int(graph[sr][sc])
 
     while(queue): 
-      curr = heapq.heappop(queue)
-      curr_dist = curr[0]
-      curr_r,curr_c = curr[1], curr[2]
+        curr_r, curr_c = queue.popleft() 
 
-      if(distance[curr_r][curr_c]<curr_dist):
-         continue 
-      
-      if(curr_r==er and curr_c==ec):
-         break
+        if(curr_r == er and curr_c==ec): 
+            break 
 
-      for i in range(4): 
-         nr = curr_r + dr[i]
-         nc = curr_c + dc[i]  
-         if(0<= nr and nr<N and 0<=nc and nc<M and curr_dist+int(graph[nr][nc])<distance[nr][nc]):
-            distance[nr][nc] = curr_dist+int(graph[nr][nc])
-            heapq.heappush(queue,(distance[nr][nc],nr,nc))
+        for i in range(4): 
+            nr = curr_r + dr[i]
+            nc = curr_c + dc[i]
 
+            if(0<=nr<N and 0<=nc<M): 
+                cost = int(graph[nr][nc])
+                if(distance[curr_r][curr_c]+cost<distance[nr][nc]):
+                    distance[nr][nc] = distance[curr_r][curr_c] + cost 
+
+                    if(cost==0):
+                        queue.appendleft((nr,nc))
+                    else: 
+                        queue.append((nr,nc))
     return distance[er][ec]
+
 
 M, N = map(int,input().split())
 graph = []
@@ -41,4 +42,4 @@ distance = [[INF for _ in range(M)] for _ in range(N)]
 for i in range(N): 
     graph.append(list(input().strip()))
 
-print(dijkstra(0,0,N-1,M-1)) 
+print(BFS01(0,0,N-1,M-1)) 
