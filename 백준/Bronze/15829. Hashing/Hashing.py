441423da08d7e81d020de772e79361f5b890b9c6
thisischeese <iamclwm@gmail.com>
2026-01-30 23:55:36 +0900

    [Bronze II] Title: Hashing, Time: 28 ms, Memory: 32412 KB, Score: 50 point -BaekjoonHub

---
+++ 백준/Bronze/15829. Hashing/Hashing.py
@@ -0,0 +1,17 @@
+import sys 
+
+R = 31
+MOD = 1234567891
+ 
+input = sys.stdin.readline 
+
+def hash(mystr):
+    answer = 0 
+    for i in range(len(mystr)):
+        answer += (ord(mystr[i])-ord('a')+1)*pow(R,i)%MOD
+    return answer
+
+
+L = int(input())
+mystr = input().strip()
+print(hash(mystr))
