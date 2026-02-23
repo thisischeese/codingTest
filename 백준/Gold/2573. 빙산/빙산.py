529ab1ce008b8d375579ab7e27af63db92542c82
thisischeese <iamclwm@gmail.com>
2026-02-23 22:37:43 +0900

    [Gold IV] Title: 빙산, Time: 2268 ms, Memory: 35860 KB -BaekjoonHub

--- 백준/Gold/2573. 빙산/빙산.py
+++ 백준/Gold/2573. 빙산/빙산.py
@@ -3,52 +3,42 @@ from collections import deque
 
 input = sys.stdin.readline 
 
-def simulation(graph):
+def simulation():
 
-    dup = [[graph[r][c] for c in range(M)] for r in range(N)]
+    global icebergs, graph,dr,dc 
 
+    melt_list = []
+    new_icebergs = [] 
 
-    sr = 0
-    sc = 0
-    cnt = 0 
+    for r,c in icebergs:
+        sea_cnt = 0 
+        for d in range(4):
+            nr = r + dr[d]
+            nc = c + dc[d]
+            if(0<=nr<N and 0<=nc<M and graph[nr][nc]==0):
+                sea_cnt += 1
+        melt_list.append(sea_cnt)
+
+    for i in range(len(icebergs)):
+        r,c = icebergs[i][0], icebergs[i][1]
+        graph[r][c] = max(graph[r][c]-melt_list[i],0)
+        if(graph[r][c]>0):
+            new_icebergs.append((r,c))
+    icebergs = new_icebergs                
+
+def solution():
     
-    for r in range(N):
-        for c in range(M):
-            if(graph[r][c]!=0): 
-                for d in range(4):
-                    nr = r + dr[d]
-                    nc = c + dc[d]
-                    if(0<=nr<N and 0<=nc<M and graph[nr][nc]==0):
-                        if(dup[r][c]>0):
-                            dup[r][c] -= 1
-    for r in range(N):
-        for c in range(M):
-            if(dup[r][c]!=0):
-                cnt += 1
-                sr = r
-                sc = c
-                
-          
-    return dup,cnt,sr,sc
-
-def solution(graph):
+    global icebergs, graph 
     time = 0 
      
-    while(True):
-        graph,cnt,sr,sc = simulation(graph)
-        # print(f"time:{time}, cnt : {cnt}")
-        # for g in graph:
-        #     print(*g)
-        
-        time += 1 
-        if(cnt==0):
-            return 0  
+    while(icebergs):
+
         
         queue = deque() 
-        queue.append((sr,sc))
+        queue.append((icebergs[0][0],icebergs[0][1]))
 
         visited = [[False for _ in range(M)] for _ in range(N)]
-        visited[sr][sc] = True
+        visited[icebergs[0][0]][icebergs[0][1]] = True
         
         total = 0 
         
@@ -59,22 +49,30 @@ def solution(graph):
             for i in range(4):
                 nr = cr + dr[i]
                 nc = cc + dc[i]       
-                if(0<=nr<N and 0<=nc<M and not visited[nr][nc] and graph[nr][nc]!=0):
+                if(0<=nr<N and 0<=nc<M and not visited[nr][nc] and graph[nr][nc]>0):
                     queue.append((nr,nc))
                     visited[nr][nc] = True 
                     
-        if(cnt!=total):
-            # print(cnt,total)
+        if(len(icebergs)!=total):
             return time
+        # 시뮬레이션 
+        simulation() 
+        time += 1
+    return 0
 
 
 
 N,M = map(int,input().split())
 graph = [list(map(int,input().split())) for _ in range(N)]
+icebergs = []
+for i in range(N):
+    for j in range(M):
+        if(graph[i][j]>0):
+            icebergs.append((i,j))
 dr = [1,-1,0,0]
 dc = [0,0,1,-1]
 
-print(solution(graph))
+print(solution())
 
 '''
 1. 1초마다 시뮬레이션 돌리기 
