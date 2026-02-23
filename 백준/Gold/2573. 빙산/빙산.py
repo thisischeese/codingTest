6cd885b3260e74fbc7bc52ad21564618a5716890
thisischeese <iamclwm@gmail.com>
2026-02-23 22:43:11 +0900

    [Gold IV] Title: 빙산, Time: 1728 ms, Memory: 35256 KB -BaekjoonHub

--- 백준/Gold/2573. 빙산/빙산.py
+++ 백준/Gold/2573. 빙산/빙산.py
@@ -2,33 +2,19 @@ import sys
 from collections import deque 
 
 input = sys.stdin.readline 
-
-def simulation():
-
-    global icebergs, graph,dr,dc 
-
-    melt_list = []
-    new_icebergs = [] 
-
-    for r,c in icebergs:
-        sea_cnt = 0 
-        for d in range(4):
-            nr = r + dr[d]
-            nc = c + dc[d]
-            if(0<=nr<N and 0<=nc<M and graph[nr][nc]==0):
-                sea_cnt += 1
-        melt_list.append(sea_cnt)
-
-    for i in range(len(icebergs)):
-        r,c = icebergs[i][0], icebergs[i][1]
-        graph[r][c] = max(graph[r][c]-melt_list[i],0)
-        if(graph[r][c]>0):
-            new_icebergs.append((r,c))
-    icebergs = new_icebergs                
+  
 
 def solution():
-    
-    global icebergs, graph 
+    N,M = map(int,input().split())
+    graph = [list(map(int,input().split())) for _ in range(N)]
+    visited = [[-1 for _ in range(M)] for _ in range(N)]
+    icebergs = []
+    for i in range(N):
+        for j in range(M):
+            if(graph[i][j]>0):
+                icebergs.append((i,j))
+    dr = [1,-1,0,0]
+    dc = [0,0,1,-1]
     time = 0 
      
     while(icebergs):
@@ -37,8 +23,7 @@ def solution():
         queue = deque() 
         queue.append((icebergs[0][0],icebergs[0][1]))
 
-        visited = [[False for _ in range(M)] for _ in range(N)]
-        visited[icebergs[0][0]][icebergs[0][1]] = True
+        visited[icebergs[0][0]][icebergs[0][1]] = time 
         
         total = 0 
         
@@ -49,28 +34,39 @@ def solution():
             for i in range(4):
                 nr = cr + dr[i]
                 nc = cc + dc[i]       
-                if(0<=nr<N and 0<=nc<M and not visited[nr][nc] and graph[nr][nc]>0):
+                if(0<=nr<N and 0<=nc<M and visited[nr][nc]!=time and graph[nr][nc]>0):
                     queue.append((nr,nc))
-                    visited[nr][nc] = True 
+                    visited[nr][nc] = time 
                     
         if(len(icebergs)!=total):
             return time
         # 시뮬레이션 
-        simulation() 
+        melt_list = []
+        new_icebergs = [] 
+    
+        for r,c in icebergs:
+            sea_cnt = 0 
+            for d in range(4):
+                nr = r + dr[d]
+                nc = c + dc[d]
+                if(0<=nr<N and 0<=nc<M and graph[nr][nc]==0):
+                    sea_cnt += 1
+            melt_list.append(sea_cnt)
+    
+        for i in range(len(icebergs)):
+            r,c = icebergs[i][0], icebergs[i][1]
+            graph[r][c] = max(graph[r][c]-melt_list[i],0)
+            if(graph[r][c]>0):
+                new_icebergs.append((r,c))
+        icebergs = new_icebergs 
+        
         time += 1
     return 0
 
 
 
-N,M = map(int,input().split())
-graph = [list(map(int,input().split())) for _ in range(N)]
-icebergs = []
-for i in range(N):
-    for j in range(M):
-        if(graph[i][j]>0):
-            icebergs.append((i,j))
-dr = [1,-1,0,0]
-dc = [0,0,1,-1]
+
+
 
 print(solution())
 
