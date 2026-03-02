af850e8eb54bea0e62de1a77ef24c2ad1354e6e3
thisischeese <iamclwm@gmail.com>
2026-03-02 18:44:20 +0900

    [Gold IV] Title: 마법사 상어와 파이어볼, Time: 240 ms, Memory: 37608 KB -BaekjoonHub

--- 백준/Gold/20056. 마법사 상어와 파이어볼/마법사 상어와 파이어볼.py
+++ 백준/Gold/20056. 마법사 상어와 파이어볼/마법사 상어와 파이어볼.py
@@ -5,73 +5,59 @@ input = sys.stdin.readline
 def solution():
     dr = [-1,-1,0,1,1,1,0,-1]
     dc = [0,1,1,1,0,-1,-1,-1]
-
-    def move():
-        temp = [[[] for _ in range(N)] for _ in range(N)]
-        for r in range(N):
-            for c in range(N):
-                for m,s,d in graph[r][c]:
-                    nr = (r + dr[d]*s)%N
-                    nc = (c + dc[d]*s)%N
-                    temp[nr][nc].append((m,s,d))
-                    
-        for r in range(N):
-            for c in range(N):
-                cnt = 0 
-                tm = 0
-                ts = 0 
-                even_cnt = 0
-                odd_cnt = 0 
-                if(len(temp[r][c])<2):
-                    continue 
-                for m,s,d in temp[r][c]:
-                    cnt += 1
-                    tm += m 
-                    ts += s 
-                    if d%2==0:
-                        even_cnt += 1
-                    else:
-                        odd_cnt += 1
-                    
-                nm = math.floor(tm/5)
-                ns = math.floor(ts/cnt)  
-                if(nm==0):
-                    temp[r][c] = []
-                    continue 
-
-                if(even_cnt==cnt or odd_cnt==cnt):
-                    td = [0,2,4,6]
-                else:
-                    td = [1,3,5,7]
-                    
-                next_balls = []
-                for nd in td:
-                    next_balls.append((nm,ns,nd))
-                temp[r][c] = next_balls
-                
-        for r in range(N):
-            for c in range(N):
-                graph[r][c] = temp[r][c]
-       
     N,M,K = map(int,input().split())
-    graph = [[[] for _ in range(N)] for _ in range(N)]
+    fireballs = [] 
     
     for _ in range(M):
         r,c,m,s,d = map(int,input().split())
-        graph[r-1][c-1].append((m,s,d))
+        # graph[r-1][c-1].append((m,s,d))
+        fireballs.append((r-1,c-1,m,s,d))
     
     for _ in range(K):
-        move()
-        # for g in graph:
-        #     print(*g)
-        
-    answer = 0
-    for r in range(N):
-        for c in range(N):
-            for m,_,_ in graph[r][c]:
-                answer += m
+        graph = {}
+        new_fireballs = []
+        for r,c,m,s,d in fireballs:
+            nr = (r + dr[d]*s)%N
+            nc = (c + dc[d]*s)%N
+            if((nr,nc) not in graph):
+                graph[(nr,nc)] = []
+            graph[(nr,nc)].append((m,s,d))
+
+        for (r,c),balls in graph.items():
+            if len(graph[(r,c)])<2:
+                for m,s,d in balls:
+                    new_fireballs.append((r,c,m,s,d))
+                continue 
+            cnt = 0 
+            tm = 0
+            ts = 0 
+            even_cnt = 0
+            odd_cnt = 0 
+            for m,s,d in balls:
+                cnt += 1
+                tm += m 
+                ts += s 
+                if d%2==0:
+                    even_cnt += 1
+                else:
+                    odd_cnt += 1
+                
+            nm = tm//5
+            ns = ts//cnt  
+            if(nm==0):
+                continue 
+
+            if(even_cnt==cnt or odd_cnt==cnt):
+                td = [0,2,4,6]
+            else:
+                td = [1,3,5,7]
+                
+            for nd in td:
+                new_fireballs.append((r,c,nm,ns,nd))
+        fireballs = new_fireballs
+
    
-    return answer 
+    return sum(ball[2] for ball in fireballs) 
 
 print(solution())
 '''
