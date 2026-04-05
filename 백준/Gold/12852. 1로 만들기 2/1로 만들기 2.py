4afe6ab555c36296ea80834f0a2cf0a2423e81a8
thisischeese <iamclwm@gmail.com>
2026-04-05 23:01:07 +0900

    [Gold V] Title: 1로 만들기 2, Time: 608 ms, Memory: 79784 KB -BaekjoonHub

--- 백준/Gold/12852. 1로 만들기 2/1로 만들기 2.py
+++ 백준/Gold/12852. 1로 만들기 2/1로 만들기 2.py
@@ -11,10 +11,10 @@ for i in range(2,N+1):
     path[i] = i-1
 
     if(i%2==0 and dp[i]>dp[i//2]+1):
-        dp[i]=min(dp[i],dp[i//2]+1)
+        dp[i]=dp[i//2]+1
         path[i] = i//2
     if(i%3==0 and dp[i]>dp[i//3]+1):
-        dp[i] = min(dp[i],dp[i//3]+1)
+        dp[i] =dp[i//3]+1
         path[i] = i//3 
         
 print(dp[N])
