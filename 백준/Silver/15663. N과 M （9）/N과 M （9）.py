2f836bf3bd886713dc44e5b915fdaf3b7d8582f5
thisischeese <iamclwm@gmail.com>
2026-04-09 17:46:06 +0900

    [Silver II] Title: N과 M (9), Time: 104 ms, Memory: 37024 KB -BaekjoonHub

--- 백준/Silver/15663. N과 M （9）/N과 M （9）.py
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.py
@@ -1,23 +1,10 @@
 import sys 
+from itertools import permutations 
 
 input = sys.stdin.readline 
 
 N,M = map(int,input().split())
-arr = list(map(int,input().split()))
-arr=sorted(arr)
-visited = [False]*N
+arr = sorted(map(int,input().split()))
 
-
-def bt(cnt,seq):
-    if cnt==M:
-        print(*seq)
-        return 
-    last = 0 
-    for idx in range(N):
-        if not visited[idx] and last!=arr[idx]:
-            visited[idx] = True
-            last = arr[idx]
-            bt(cnt+1,seq+[arr[idx]])
-            visited[idx] = False 
-
-bt(0,[])
+for comb in sorted(set(permutations(arr,M))):
+    print(*comb)
