85355c544c9a9ce8549b993436b16a314e540dbc
thisischeese <iamclwm@gmail.com>
2026-04-11 22:14:29 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 304 ms, Memory: 53052 KB -BaekjoonHub

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.py
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.py
@@ -5,7 +5,7 @@ input = sys.stdin.readline
 
 N = int(input())
 adj = [[] for _ in range(N+1)]
-parents = [i for i in range(N+1)]
+parents = [0 for i in range(N+1)]
 
 for _ in range(N-1):
     s,e = map(int,input().split())
@@ -13,17 +13,16 @@ for _ in range(N-1):
     adj[s].append(e) 
 
 def bfs(start):
-    visited = [False]*(N+1)
-    visited[start] = True 
+    parents[start] = start 
     queue = deque([start])
     
     while(queue):
         curr = queue.popleft() 
         for next in adj[curr]:
-            if not visited[next]:
-                visited[next] = True 
-                queue.append(next)
+            if parents[next]==0:
                 parents[next] = curr 
+                queue.append(next)
+                
 bfs(1) 
 print(*parents[2:],sep='\n')
     
