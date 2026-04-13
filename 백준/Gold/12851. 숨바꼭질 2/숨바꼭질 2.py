94b6f063de18317b7007a9cad190848d8593b196
thisischeese <iamclwm@gmail.com>
2026-04-13 16:24:27 +0900

    [Gold IV] Title: 숨바꼭질 2, Time: 392 ms, Memory: 49676 KB -BaekjoonHub

--- 백준/Gold/12851. 숨바꼭질 2/숨바꼭질 2.py
+++ 백준/Gold/12851. 숨바꼭질 2/숨바꼭질 2.py
@@ -6,26 +6,25 @@ input = sys.stdin.readline
 N,K = map(int,input().split())
 
 def find(curr):
+    answers = [0]*100001
+    answers[curr] = 1
+    
     visited = [-1]*100001
     visited[curr] = 0 
     
     queue = deque()
     queue.append((curr,0)) 
     
-    answer = 0 
-    
     while(queue):
         curr,time = queue.popleft() 
-        if(curr==K):
-            answer += 1 
-            continue 
         
         for next in (curr*2,curr-1,curr+1):
             if(0<=next<=100000 and (visited[next]==-1 or visited[next]==time+1)):
                 queue.append((next,time+1))
                 visited[next] = time+1 
+                answers[next] +=1 
                 
-    return visited[K], answer 
+    return visited[K], answers[K]
                 
 time,methods = find(N)
 print(time)
