531463eaea08ad09b6ce32f5f0b8e411c89115a8
thisischeese <iamclwm@gmail.com>
2026-04-13 15:25:15 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 116 ms, Memory: 36376 KB -BaekjoonHub

--- 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.py
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.py
@@ -1,32 +1,26 @@
-import sys
-from collections import deque
+import sys 
+from collections import deque 
 
-input = sys.stdin.readline
+input = sys.stdin.readline 
 
-def BFS(N, K):
-    max_pos = 100001
-    visited = [False] * max_pos
+N,K = map(int,input().split())
+
+def find(curr):
+    visited = [False]*(100001)
+    visited[curr] = True
+    
     queue = deque()
-    queue.append((N, 0))
-    visited[N] = True
+    queue.append((curr,0))
+    
+    while(queue):
+        curr,time = queue.popleft() 
+        if(curr==K):
+            return time
+        nexts = [curr*2,curr-1,curr+1]
+        diff = [0,1,1]
+        for i in range(len(nexts)):
+            if(0<=nexts[i]<=100000 and not visited[nexts[i]]):
+                visited[nexts[i]] = True 
+                queue.append((nexts[i],time+diff[i]))
 
-    while queue:
-        curr_pos, curr_time = queue.popleft()
-
-        if curr_pos == K:
-            return curr_time
-
-        # 순간이동 (0초 소요): 큐 앞에 삽입
-        next_pos = curr_pos * 2
-        if 0 <= next_pos < max_pos and not visited[next_pos]:
-            visited[next_pos] = True
-            queue.appendleft((next_pos, curr_time))
-
-        # 걷기 (-1, +1): 1초 소요
-        for next_pos in [curr_pos - 1, curr_pos + 1]:
-            if 0 <= next_pos < max_pos and not visited[next_pos]:
-                visited[next_pos] = True
-                queue.append((next_pos, curr_time + 1))
-
-N, K = map(int, input().split())
-print(BFS(N, K))
+print(find(N))
