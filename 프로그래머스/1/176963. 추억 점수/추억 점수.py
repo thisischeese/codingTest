e7322373ae8b2ba3af319710f9f899fbc8841834
thisischeese <iamclwm@gmail.com>
2026-08-05 16:28:14 +0900

    [level 1] Title: 추억 점수, Time: 0.57 ms, Memory: 12 MB -BaekjoonHub

--- 프로그래머스/1/176963. 추억 점수/추억 점수.py
+++ 프로그래머스/1/176963. 추억 점수/추억 점수.py
@@ -1,14 +1,6 @@
-from collections import defaultdict
-
 def solution(name, yearning, photos):
-    answer = [0 for _ in range(len(photos))]
-    score = defaultdict(int)
+    score = dict(zip(name,yearning))
     
-    for i in range(len(name)):
-        score[name[i]] = yearning[i]
-        
-    for p_idx in range(len(photos)):
-        for j in range(len(photos[p_idx])):
-            answer[p_idx] += score[photos[p_idx][j]]
-
+    answer = [sum(score.get(person,0) for person in photo) \
+              for photo in photos]
     return answer
