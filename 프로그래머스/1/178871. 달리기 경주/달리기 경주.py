07d291924bec4ad993fd98dc69e879949b25b718
thisischeese <iamclwm@gmail.com>
2026-08-07 22:11:40 +0900

    [level 1] Title: 달리기 경주, Time: 413.22 ms, Memory: 73.4 MB -BaekjoonHub

--- 프로그래머스/1/178871. 달리기 경주/달리기 경주.py
+++ 프로그래머스/1/178871. 달리기 경주/달리기 경주.py
@@ -1,8 +1,8 @@
 def solution(names, callings):
     # 선수 : 순서 
     players = {names[i]:i for i in range(len(names))}
-    # 순서 : 선수 
-    orders = {i:names[i] for i in range(len(names))}
+    # 순서 : 선수 -> idx가 이미 순서 역할 수행 
+    orders = names[:]
     
     for player in callings:
         # 현재 순서 정보 조회 
@@ -17,4 +17,4 @@ def solution(names, callings):
         players[player] = order-1 
         players[prev_player] = order 
 
-    return list(dict(sorted(orders.items())).values())
+    return orders
