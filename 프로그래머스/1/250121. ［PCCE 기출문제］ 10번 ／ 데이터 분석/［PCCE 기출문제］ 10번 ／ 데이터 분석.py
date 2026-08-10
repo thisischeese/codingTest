42e422a2e9270e92d1cd95167a7d094cefe55d37
thisischeese <iamclwm@gmail.com>
2026-08-10 20:26:44 +0900

    [level 1] Title: [PCCE 기출문제] 10번 / 데이터 분석, Time: 0.09 ms, Memory: 11.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/250121. ［PCCE 기출문제］ 10번 ／ 데이터 분석/［PCCE 기출문제］ 10번 ／ 데이터 분석.py
@@ -0,0 +1,3 @@
+def solution(data, ext, val_ext, sort_by):
+    idx_dict = {"code":0, "date":1, "maximum":2, "remain":3}
+    return sorted([d for d in data if d[idx_dict[ext]]<val_ext],key=lambda x : x[idx_dict[sort_by]])
