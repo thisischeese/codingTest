d7c0fe13e8e96c7993af67dc8991313429d64ef3
thisischeese <iamclwm@gmail.com>
2026-08-13 23:06:07 +0900

    [level 1] Title: 나머지가 1이 되는 수 찾기, Time: 0.04 ms, Memory: 11.4 MB -BaekjoonHub

--- 프로그래머스/1/87389. 나머지가 1이 되는 수 찾기/나머지가 1이 되는 수 찾기.py
+++ 프로그래머스/1/87389. 나머지가 1이 되는 수 찾기/나머지가 1이 되는 수 찾기.py
@@ -1,7 +1,8 @@
+import math
 def solution(n):
-    for i in range(2,n):
-        if(n-1)%i==0:
-            return i
+    for i in range(2,int(math.sqrt(n))+1):
+        if(n-1)%i==0: return i
+    return n-1
 
 '''
 사실 (n-1)의 약수 중 1보다 큰 것 찾기  
