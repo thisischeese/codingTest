3d9e1930d57c525d2926f1ea2a4fe895cd1965ef
thisischeese <iamclwm@gmail.com>
2026-08-24 15:59:51 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.03 ms, Memory: 11.6 MB -BaekjoonHub

--- 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.py
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.py
@@ -1,15 +1,7 @@
 def solution(words):
-    words = list(words)
-    for i in range(len(words)):
-        if((i==0 or (i>0 and ord(words[i-1])==32)) and 97<=ord(words[i])<=122):
-            words[i] = chr(ord(words[i])-32)
-        elif((i>0 and ord(words[i-1])!=32) and 65<=ord(words[i])<=90):
-            words[i] = chr(ord(words[i])+32)
-    return "".join(words)
+    return " ".join(word.capitalize() for word in words.split(" "))
     
 """
 1. 처음부터 문자열 스캔하기
-2. 만약 문자가 공백이거나 숫자이다 -> 그대로 추가
-3. 만약 문자가 소문자인데 이전 문자가 공백이거나 인덱스가 0이다 -> 대문자로 바꾸기
-4. 만약 문자가 대문자인데 이전 문자가 공백이 아니다 -> 소문자로 바꾸기 
+2. capitalize 수행하기 
 """
--- 프로그래머스/2/12951. JadenCase 문자열 만들기/README.md
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/README.md
@@ -1,10 +1,10 @@
 # [level 2] JadenCase 문자열 만들기 - 12951 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12951#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12951) 
 
 ### 성능 요약
 
-메모리: 11.3 MB, 시간: 0.04 ms
+메모리: 11.6 MB, 시간: 0.03 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 08월 24일 15:51:31
+2026년 08월 24일 15:59:47
 
 ### 문제 설명
 
