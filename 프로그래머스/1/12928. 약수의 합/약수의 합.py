edf955c1d455b15215f3212428c526a4998f5778
thisischeese <iamclwm@gmail.com>
2026-08-27 23:59:52 +0900

    [level 1] Title: 약수의 합, Time: 0.01 ms, Memory: 11.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12928. 약수의 합/약수의 합.py
@@ -0,0 +1,10 @@
+import math
+
+def solution(n):
+    answer = 0
+    for i in range(1, int(math.sqrt(n)) + 1):
+        if n % i == 0:
+            answer += i
+            if i != n // i:
+                answer += n // i
+    return answer
